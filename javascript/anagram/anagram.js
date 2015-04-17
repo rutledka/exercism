343dbe8d4e2c28e76fc0d2cdede256119c51d845
Khalil Rutledge <khalil@airtightdesign.com>
2015-04-17 11:32:57 -0400

    finished anagram

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,52 +1,66 @@
 var Anagram = function(input){
-  self = this;
-  self.input = input;
-  self.output = [];
-  self.matches = function(input) {
-    for(var i=0; i < input.length; i++){
-    	var match = false;
-		if(input[i].toLowerCase() !== self.input.toLowerCase()) {
-			var obj1 = self.letterCount(self.input);
-			var obj2 = self.letterCount(input[i]);
-			
-			if( compareObjects(obj1,obj2) ) {
-				match = true;
-				//console.log("we got a match");
+	self = this;
+	self.input = input;
+
+	self.matches = function() {
+		var args = [],
+			output =[];
+
+		//convert our arguments object from any combo of strings and arrays into one array
+		for( var i=0; i < arguments.length; i++ ){
+			if( typeof arguments[i] === 'object' ) {
+				args = args.concat(arguments[i]);
+			} else {
+				args.push(arguments[i]);
 			}
 		}
 
-		if(match) {
-			self.output.push(input[i]);
-		}
-    }
-    
-    return self.output;
-  };
+		//loop through args array and compares elements to our input
+		for(var i=0; i < args.length; i++){
+			var match = false;
+			if(args[i].toLowerCase() !== self.input.toLowerCase()) {
+				var obj1 = self.letterCount(self.input);
+				var obj2 = self.letterCount(args[i]);
+				
+				if( compareObjects(obj1,obj2) ) {
+					match = true;
+				}
+			}
 
-  self.letterCount = function(word) {
-	var values = [],
-		numLetters = {},
-		word = word.toLowerCase();
-
-	for(var i = 0; i < word.length; i++){
-		values[i] = [word[i],0];
-		for(var j = 0; j < word.length; j++){
-			if(word[i] === word[j]){
-				values[i][1]++;
+			if(match) {
+				output.push(args[i]);
 			}
 		}
-	}
 
-	for(var i = 0; i < values.length; i++) {
-		numLetters[values[i][0]] = values[i][1];
-	}
+	return output;
+	};
 
-	return numLetters;
-  }
+	//accepts input as string. returns an object with letters as the properties and the number of occurences as the values
+	self.letterCount = function(word) {
+		var values = [],
+			numLetters = {},
+			word = word.toLowerCase();
+
+		for(var i = 0; i < word.length; i++){
+			values[i] = [word[i],0];
+			for(var j = 0; j < word.length; j++){
+				if(word[i] === word[j]){
+					values[i][1]++;
+				}
+			}
+		}
+
+		for(var i = 0; i < values.length; i++) {
+			numLetters[values[i][0]] = values[i][1];
+		}
+
+		return numLetters;
+	}
 
   return this;
 };
 
+// compares two objects. returns true if they have the same property/value pairs, regardless of order
 var compareObjects = function(o1, o2){
     for(var prop in o1){
         if(o1.hasOwnProperty(prop)){
@@ -66,6 +80,3 @@ var compareObjects = function(o1, o2){
 };
 
 module.exports = Anagram;
-
-var A = new Anagram("title");
-
--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -3,14 +3,14 @@ var compute = function(string1 , string2){
 	
 	if( string1.length !== string2.length ){
 		throw new Error("DNA strands must be of equal length.");	
-	} else {
-		for(var i=0; i<string1.length; i++ ){
-			if(string1[i] !== string2[i]){
-				distance++;
-			}
-		}
-		return distance;
 	}
+	
+	for(var i=0; i<string1.length; i++ ){
+		if(string1[i] !== string2[i]){
+			distance++;
+		}
+	}
+	return distance;
 };
 
 module.exports = compute;
